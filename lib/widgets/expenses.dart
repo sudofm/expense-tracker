351e367999c6e1deca5141a22626a4b651c90e71
faciquidoumbia <fmdoums14@gmail.com>
2023-12-18 20:11:53 +0100

    adding appBar button and Category icons for expenses

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expenses.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
@@ -29,9 +30,24 @@ class _ExpensesState extends State<Expenses> {
         date: DateTime.now(),
         category: Category.food),
   ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => const NewExpense());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter ExpenseTracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: Icon(Icons.add),
+          )
+        ],
+      ),
       body: Column(children: [
         const Text('The Chart'),
         Expanded(child: ExpensesList(expenses: _registeredExpenses))
