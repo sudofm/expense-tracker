a477d7c67f710f7b4c2b8c5450fd5b9475c50b30
faciquidoumbia <fmdoums14@gmail.com>
2023-12-26 22:19:34 +0100

    adding new input for amount and cancel button

--- lib/widgets/new_expenses.dart
+++ lib/widgets/new_expenses.dart
@@ -10,17 +10,61 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  // var _enteredTitle = '';
+
+  // void _saveTitleInput(String inputValue) {
+  //   _enteredTitle = inputValue;
+  // }
+
+  final _titleControler = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleControler.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.all(16),
       child: Column(children: [
         TextField(
+          //onChanged: _saveTitleInput,
+          controller: _titleControler,
           maxLength: 50,
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             label: Text('Title'),
           ),
         ),
+        TextField(
+          controller: _amountController,
+          keyboardType: TextInputType.number,
+          decoration: const InputDecoration(
+            prefixText: '€ ',
+            label: Text('Amount'),
+          ),
+        ),
+        Row(
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                //print(_enteredTitle);
+                print(_titleControler.text);
+                print(_amountController.text);
+              },
+              child: const Text('Save Expense'),
+            ),
+          ],
+        )
       ]),
     );
   }
